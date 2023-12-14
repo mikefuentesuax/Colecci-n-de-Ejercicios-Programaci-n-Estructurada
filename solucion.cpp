82b96018cf218ebf730f8043af9fc9a5292ddde1
mikefuentesuax <146821555+mikefuentesuax@users.noreply.github.com>
2023-12-14 13:28:22 +0000

    codigo completo

--- solucion.cpp
+++ solucion.cpp
@@ -22,6 +22,56 @@ int main(){
     Estudiante* punteroEst = &estudiante1;
     cout << "Nombre: " << punteroEst->nombre << ", Edad: " << punteroEst->edad << ", Promedio: " << punteroEst->promedio << endl;
 
+    union Dato {
+        int i;
+        float f;
+        char str[20];
+    };
+
+    Dato dato;
+
+    dato.i = 10;    
+    cout << "dato.i: " << dato.i << endl;
+
+    dato.f = 220.5;
+    cout << "dato.f: " << dato.f << endl;
+
+    strcpy(dato.str, "C++");
+    cout << "dato.str: " << dato.str << endl;
+
+    Estudiante copiarEstudiante(Estudiante est){
+        Estudiante nuevoEst;
+        strcpy(nuevoEst.nombre, est.nombre);
+        nuevoEst.edad = est.edad;
+        nuevoEst.promedio = est.promedio;
+        return nuevoEst;
+        }
+
+        typedef Estudiante Estudiante;
+
+        Estudiante estudiante3;
+        strcpy(estudiante3.nombre, "Maria");
+        estudiante3.edad = 22;
+        estudiante3.promedio = 17.5;
+
+        auto imprimirEstudiante = [](Estudiante est){
+            cout << "Nombre: " << est.nombre << ", Edad: " << est.edad << ", Promedio: " << est.promedio << endl;
+        };
+
+        imprimirEstudiante(estudiante1);
+
+        auto modificarEstudiante = [](Estudiante &est){
+            est-> edad = 25;
+        };
+        modificarEstudiante(estudiante1);
+
+        auto imprimirEstudiante2 = [](Estudiante est){
+            cout << "Nombre: " << est.nombre << ", Edad: " << est.edad << ", Promedio: " << est.promedio << endl;
+        };
+        imprimirEstudiante2(estudiante1);
+
+        delete estudiante2;
+
 
 
     return 0;
